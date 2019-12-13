a0a0de8d8eeeedc89ff1264b078aa9946136009b
Anudeep Chandra Paul <achandrapaul@manh.com>
2019-12-13 18:18:47 +0530

    [Initial Commit] Project created

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Anudeep Chandra Paul on 12/12/19.
+//  Copyright © 2019 Anudeep Chandra Paul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
